af6d3b5c621a7784446295e153cb30a9b6f4e07f
sachin diwakar <sachindiwakar1802@gmail.com>
2026-03-17 16:43:36 +0530

    code

--- backend/middlwares/isAuth.js
+++ backend/middlwares/isAuth.js
@@ -1,3 +1,18 @@
-const isAuth = async (req,res,next)=>{
-  try
-}
+const isAuth = (req, res, next) => {
+  try {
+    const token = req.cookies.token;
+
+    if (!token) {
+      return res.status(401).json({ message: "Token not found" });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    // attach user info to request
+    req.userId = decoded.id;
+
+    next(); // move to next middleware/controller
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid or expired token" });
+  }
+};
