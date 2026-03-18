5dd9e5ded98080fb51d25efbc99f86bc9de66685
sachin diwakar <sachindiwakar1802@gmail.com>
2026-03-18 18:29:19 +0530

    code

---
+++ frontend/src/redux/userSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    userData: null,
+  },
+  reducers: {
+    setUserData: (state, action) => {
+      state.userData = action.payload;
+    },
+  },
+});
+
+// export actions
+export const { setUserData } = userSlice.actions;
+
+// export reducer
+export default userSlice.reducer;
